9e7e2a76f0e227bc604f586917d1b7e41d6a3ba3
ugp <ugp@ugps-MacBook-3.local>
2015-07-15 18:35:53 -0500

    added time series file

---
+++ time_series.py
@@ -0,0 +1,12 @@
+# WILL COME BACK TO LATER
+
+import pandas as pd 
+import numpy as np
+import statsmodels.api as sm
+
+# Plot out the ACF:
+statsmodels.api.graphics.tsa.plot_acf()
+
+# PACF: 
+statsmodels.api.graphics.tsa.plot_pacf()
+
